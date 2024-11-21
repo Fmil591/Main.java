02ffa566019d45f20acf52a9442c29aa5eefa281
Emil <emweiss@tsn.at>
2024-11-21 14:57:37 +0100

    Video 3 beendet, Verbindung herstellen

--- src/main/java/at/itkollegimst/Main.java
+++ src/main/java/at/itkollegimst/Main.java
@@ -1,7 +1,22 @@
 package at.itkollegimst;
 
+import dataaccess.MysqlDatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TEST");
+
+
+        try {
+            Connection myConnection =
+                    MysqlDatabaseConnection.getConnection("jdbc:mysql://localhost:3306/imstkurssystem","root","root");
+            System.out.println("Verbindung aufgebaut");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
