898d6032a3a13c0d15cc3ae455c4125421ddb97e
Emil <emweiss@tsn.at>
2024-12-10 16:29:33 +0100

    Video 10 fertig, funktioniert

--- src/main/java/scratch_1.md
+++ src/main/java/scratch_1.md
@@ -1,4 +1,4 @@
 6.12 Video 5 fertig
 7.12 Video 6 fertig
 9.12 Video 8 fertig gemacht, Detailsanzeigen fertig programmiert, Fehler ausgebessert, getestet, funktioniert
-10.12 Video 9 fetrig gemacht, getestet, nun kann man Kurse hinzufügen in IntelliJ
+10.12 Video 9 fetrig gemacht, getestet, nun kann man Kurse hinzufügen in IntelliJ, Video 10 fertig, Kurse abäbnder jetzt möglich
--- src/main/java/ui/Cli.java
+++ src/main/java/ui/Cli.java
@@ -42,6 +42,9 @@ public class Cli {
                 case "3":
                     showCourseDetails();
                     break;
+                case "4":
+                    updateCourseDetails();
+                    break;
                 case "x":
                     System.out.println("Auf Wiedersehen");
                     break;
@@ -51,6 +54,62 @@ public class Cli {
             }
         }
 scan.close();
+    }
+
+    private void updateCourseDetails() {
+        System.out.println("Für welche Kurs-ID möchten Sie die Kusdetails ändern?");
+        Long courseId = Long.parseLong(scan.nextLine());
+
+        try {
+            Optional<Course> courseOptional = repo.getById(courseId);
+            if(courseOptional.isEmpty()){
+                System.out.println("Kurs mit der gegebenen ID nicht in der Datenbank!");
+            } else{
+                Course course = courseOptional.get();
+
+                System.out.println("Änderungen für folgenden Kurs: ");
+                System.out.println(course);
+
+                String name, description, hours, dateFrom, dateTo, courseTipe;
+
+                System.out.println("Bitte neue Kursdaten angeben (Enter, falss keine Änderung gewünscht ist):");
+                System.out.println("Name: ");
+                name = scan.nextLine();
+                System.out.println("Beschreibung ");
+                description = scan.nextLine();
+                System.out.println("Stundenanzahl");
+                hours = scan.nextLine();
+                System.out.println("Startdatum (YYYY-MM-DD): ");
+                dateFrom = scan.nextLine();
+                System.out.println("Enddate (YYYY-MM-DD): ");
+                dateTo = scan.nextLine();
+                System.out.println("Kurstyp (ZA/BF/FF/OE): ");
+                courseTipe = scan.nextLine();
+
+
+                Optional<Course> optionalCourseUpdated = repo.update(
+                        new Course(
+                                course.getId(),
+                                name.equals("") ? course.getName() : name,
+                                description.equals("") ? course.getDescription():description,
+                                hours.equals("") ? course.getHours():Integer.parseInt(hours),
+                                dateFrom.equals("") ? course.getBeginDate():Date.valueOf(dateFrom),
+                                dateTo.equals("") ? course.getEndDate(): Date.valueOf(dateTo),
+                                courseTipe.equals("")?course.getCourseType():CourseType.valueOf(courseTipe)
+                        )
+                );
+
+                optionalCourseUpdated.ifPresentOrElse(
+                        (c)-> System.out.println("Kurs aktualisiert: " + c),
+                        ()-> System.out.println("Kurs konnte nicht aktualisiert werden!")
+                );
+
+
+            }
+        } catch (Exception exception)
+        {
+            System.out.println("Unbekannter Fehler bei Kursupdate: " + exception.getMessage());
+        }
     }
 
     private void addCourse() {
@@ -143,6 +202,7 @@ scan.close();
     {
         System.out.println("-----------------------------------------------------KURSMANAGEMENT---------------------------");
         System.out.println("(1) Kurs eingeben \t (2) Alle Kurse anzeigen \t" + "(3) Kursdetails anzeigen");
+        System.out.println("(4) Kursdetails ändern \t (-) xxx \t" + "(-) xxx");
         System.out.println("(x) ENDE");
     }
 
